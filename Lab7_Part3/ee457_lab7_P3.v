cd0670fed7ebee01e29dd8e0842f8af195768fc7
chaoyij <chaoyij@usc.edu>
2023-03-27 19:58:32 -0700

    Implement pipelined design

--- Lab7_Part3/ee457_lab7_P3.v
+++ Lab7_Part3/ee457_lab7_P3.v
@@ -85,14 +85,14 @@ wire [31:0] WB_INSTR;
 
 //  Task #3 Complete the enable pin connection on the PC
 // module pc (en,clk,rstb,pco);
-pc PC(.en(complete_it),.clk(CLK),.rstb(RSTB),.pco(PC_OUT[7:0]));
+pc PC(.en(STALL_B),.clk(CLK),.rstb(RSTB),.pco(PC_OUT[7:0]));
 //--IF-----------------------------------------------------------------
 // module ins_mem(a,d_out);
 ins_mem INS_MEM(.a(PC_OUT[5:0]),.d_out(IF_INSTR));
 
 //  Task #4 Complete the enable pin connection on the IF_ID register
 // module pipe_if_id_P3(rstb,clk,en,id_xa,id_ra,add1,add4,sub3,mov,instr_in,instr_out);
-pipe_if_id_P3 IF_ID(.rstb(RSTB),.clk(CLK),.en(complete_it),.id_xa(ID_XA),.id_ra(ID_RA),
+pipe_if_id_P3 IF_ID(.rstb(RSTB),.clk(CLK),.en(STALL_B),.id_xa(ID_XA),.id_ra(ID_RA),
                     .add1(ID_ADD1),.add4(ID_ADD4),.sub3(ID_SUB3),.mov(ID_MOV),.instr_in(IF_INSTR),.instr_out(ID_INSTR));
 //--ID-----------------------------------------------------------------
 
@@ -101,8 +101,8 @@ pipe_if_id_P3 IF_ID(.rstb(RSTB),.clk(CLK),.en(complete_it),.id_xa(ID_XA),.id_ra(
 //  the signals in the ID stage or the signals in WB stage.
 
 // module register_file (r1a,r2a,r3a,wa,wd,reg_write,r1d,r2d,r3d,clk);
-register_file REG_FILE(.r1a(4'b0000),.r2a(4'b0000),.r3a(ID_XA),.wa(complete_it),.wd(complete_it),
-                       .reg_write(complete_it),.r1d(),.r2d(),.r3d(ID_XD),.clk(CLK));
+register_file REG_FILE(.r1a(4'b0000),.r2a(4'b0000),.r3a(ID_XA),.wa(WB_RA),.wd(WB_RD),
+                       .reg_write(WB_WRITE),.r1d(),.r2d(),.r3d(ID_XD),.clk(CLK));
 
 // module comp_station_P3(ex2_ra,ex1_ra,id_xa,xmex2,xmex1);
 comp_station_P3 COMP_STATION (.ex2_ra(EX2_RA),.ex1_ra(EX1_RA),.id_xa(ID_XA),
@@ -110,12 +110,12 @@ comp_station_P3 COMP_STATION (.ex2_ra(EX2_RA),.ex1_ra(EX1_RA),.id_xa(ID_XA),
 
 //  Task #2 Complete the following continuous assign statements describing the combinational 
 //  logic in ID stage to produce stall and also inject a bubble into EX1 stage if needed 							  
-assign STALL =                ;
+assign STALL = (ID_XMEX1) & (ID_SUB3 | ID_ADD1) & (EX1_ADD4 | EX1_ADD1);
 assign STALL_B = ~ STALL;
-assign ID_MOV_OUT  = (         ) & (ID_MOV);
-assign ID_SUB3_OUT = (         ) & (ID_SUB3);
-assign ID_ADD4_OUT = (         ) & (ID_ADD4);
-assign ID_ADD1_OUT = (         ) & (ID_ADD1);
+assign ID_MOV_OUT  = (STALL_B) & (ID_MOV);
+assign ID_SUB3_OUT = (STALL_B) & (ID_SUB3);
+assign ID_ADD4_OUT = (STALL_B) & (ID_ADD4);
+assign ID_ADD1_OUT = (STALL_B) & (ID_ADD1);
 assign ID_INSTR_or_BUBBLE = STALL ? 
 		{ID_MOV_OUT,ID_SUB3_OUT,ID_ADD4_OUT,ID_ADD1_OUT,20'hFFFFF,ID_INSTR[7:0]} // 20'hFFFFF lets the testbench distinguish a bubble from a NOP
 		: 
@@ -128,7 +128,7 @@ assign ID_INSTR_or_BUBBLE = STALL ?
 // 		vec16_in1,vec16_in2,vec16_in3,vec16_out1,vec16_out2,vec16_out3,
 // 		vec4_in1,vec4_in2,vec4_out1,vec4_out2,
 // 		bit_in1,bit_in2,bit_in3,bit_in4,bit_in5,bit_in6,bit_out1,bit_out2,bit_out3,bit_out4,bit_out5,bit_out6,instr_in,instr_out);
-pipe_reg2 ID_EX1(.rstb(RSTB),.clk(CLK),.en(complete_it),
+pipe_reg2 ID_EX1(.rstb(RSTB),.clk(CLK),.en(1'b1),
 	.vec16_in1(16'h0000),.vec16_in2(16'h0000),.vec16_in3(ID_XD),.vec16_out1(),.vec16_out2(),.vec16_out3(EX1_XD),
 	.vec4_in1(ID_RA),.vec4_in2(4'b0000),.vec4_out1(EX1_RA),.vec4_out2(),
 	.bit_in1(ID_MOV_OUT),.bit_in2(ID_SUB3_OUT),.bit_in3(ID_ADD4_OUT),.bit_in4(ID_ADD1_OUT),.bit_in5(ID_XMEX1),.bit_in6(ID_XMEX2),
@@ -140,13 +140,13 @@ pipe_reg2 ID_EX1(.rstb(RSTB),.clk(CLK),.en(complete_it),
 //  Task #6 Complete the following continuous assign statements describing the combinational 
 //  logic in EX1 stage to produce PRIORITY, FORW1, and SKIP1 and use them to select data. 
 
-assign PRIORITY =  						; // Priority between forwarding help from the EX2 stage and the WB stage
-assign FORW1 =   						; // Decide whether to receive any forwarding help or carry EX1_XD
-assign EX1_PRIO_XD = PRIORITY ?  		; // EX1_PRIO_XD = The output the mux controlled by the selection line PRIORITY
-assign EX1_ADDER_IN = FORW1 ?  			; // EX1_ADDER_IN = The output the forwarding mux which is the input to the adder in EX1
+assign PRIORITY = (EX1_XMEX1) & (EX2_SUB3 | EX2_MOV); // Priority between forwarding help from the EX2 stage and the WB stage
+assign FORW1 = (PRIORITY) | (EX1_XMEX2 & WB_WRITE); // Decide whether to receive any forwarding help or carry EX1_XD
+assign EX1_PRIO_XD = PRIORITY ? EX2_XD : WB_RD; // EX1_PRIO_XD = The output the mux controlled by the selection line PRIORITY
+assign EX1_ADDER_IN = FORW1 ? EX1_PRIO_XD : EX1_XD; // EX1_ADDER_IN = The output the forwarding mux which is the input to the adder in EX1
 assign EX1_ADDER_OUT = EX1_ADDER_IN + (-3); // Subtract 3
-assign SKIP1 =                          ; // Decide whether to skip the subtractor result
-assign EX1_XD_OUT = SKIP1 ?             ; // mux to skip the subtractor result, (i.e. select between EX1_ADDER_IN  EX1_ADDER_OUT) Note: EX1_XD_OUT = Data going out of EX1
+assign SKIP1 = EX1_MOV | EX1_ADD4;          // Decide whether to skip the subtractor result
+assign EX1_XD_OUT = SKIP1 ? EX1_ADDER_IN : EX1_ADDER_OUT; // mux to skip the subtractor result, (i.e. select between EX1_ADDER_IN  EX1_ADDER_OUT) Note: EX1_XD_OUT = Data going out of EX1
 
 //  Task #7 Complete the enable pin connection on the EX1_EX2 register  .en(complete_it)
 //  If you want to keep it enabled permanently, you can do: .en(1'b1)
@@ -157,7 +157,7 @@ assign EX1_XD_OUT = SKIP1 ?             ; // mux to skip the subtractor result, 
 // 		vec16_in1,vec16_in2,vec16_in3,vec16_out1,vec16_out2,vec16_out3,
 // 		vec4_in1,vec4_in2,vec4_out1,vec4_out2,
 // 		bit_in1,bit_in2,bit_in3,bit_in4,bit_in5,bit_in6,bit_out1,bit_out2,bit_out3,bit_out4,bit_out5,bit_out6,instr_in,instr_out);
-pipe_reg2 EX1_EX2(.rstb(RSTB),.clk(CLK),.en(complete_it),
+pipe_reg2 EX1_EX2(.rstb(RSTB),.clk(CLK),.en(1'b1),
 	.vec16_in1(16'h0000),.vec16_in2(16'h0000),.vec16_in3(EX1_XD_OUT),.vec16_out1(),.vec16_out2(),.vec16_out3(EX2_XD),
 	.vec4_in1(EX1_RA),.vec4_in2(4'b0000),.vec4_out1(EX2_RA),.vec4_out2(),
 	.bit_in1(EX1_MOV),.bit_in2(EX1_SUB3),.bit_in3(EX1_ADD4),.bit_in4(EX1_ADD1),.bit_in5(complete_it),.bit_in6(1'b0),
@@ -169,12 +169,12 @@ pipe_reg2 EX1_EX2(.rstb(RSTB),.clk(CLK),.en(complete_it),
 //  Task #8 Complete the following continuous assign statements describing the combinational 
 //  logic in EX2 stage to produce FORW2 and SKIP2 and use them to select data. 
 
-assign FORW2 =                               ;  // Is it a case of forwarding date from the WB stage
-assign EX2_ADDER_IN = FORW2 ?       :        ;  // select between EX2_XD and WB_BD
+assign FORW2 = (EX2_XMEX1) & (EX2_MOV | EX2_ADD4) & (WB_WRITE);  // Is it a case of forwarding date from the WB stage
+assign EX2_ADDER_IN = FORW2 ? WB_RD : EX2_XD;  // select between EX2_XD and WB_BD
 assign EX2_ADDER_OUT = EX2_ADDER_IN + (+4);     // Add 4
-assign SKIP2 =                               ;  // Is it a case of skipping the ADD4 operation
-assign EX2_XD_OUT = SKIP2 ?         :        ;  // Skip add4 result if needed (i.e. select between EX2_ADDER_IN  EX2_ADDER_OUT)
-assign EX2_WRITE =                           ;  // Produce one signal out of EX2_MOV,EX2_SUB3,EX2_ADD4,EX2_ADD1
+assign SKIP2 = EX2_MOV | EX2_SUB3;              // Is it a case of skipping the ADD4 operation
+assign EX2_XD_OUT = SKIP2 ? EX2_ADDER_IN : EX2_ADDER_OUT;  // Skip add4 result if needed (i.e. select between EX2_ADDER_IN  EX2_ADDER_OUT)
+assign EX2_WRITE = EX2_MOV | EX2_SUB3 | EX2_ADD4 | EX2_ADD1;  // Produce one signal out of EX2_MOV,EX2_SUB3,EX2_ADD4,EX2_ADD1
 
 
 //  Task #9 Complete the enable pin connection on the EX2_WB register  .en(complete_it)
@@ -191,7 +191,7 @@ pipe_reg2 EX2_WB(.rstb(RSTB),.clk(CLK),.en(1'b1),
 	.bit_in1(complete_it),.bit_in2(1'b0),.bit_in3(1'b0),.bit_in4(1'b0),.bit_in5(1'b0),.bit_in6(1'b0),
 	.bit_out1(WB_WRITE),.bit_out2(),.bit_out3(),.bit_out4(),.bit_out5(),.bit_out6(),
 	.instr_in(EX2_INSTR),.instr_out(WB_INSTR)
-	);    
+	);
 //--WB-----------------------------------------------------------------
 // There is no explicit logic here. 
 // The writing associated with the WB is taken care in the coding of the register file
